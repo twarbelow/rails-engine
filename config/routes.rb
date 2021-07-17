c1b759deb0678ea9bac01016f00bd3eed8394ac1
twarbelow <taija.warbelow@gmail.com>
2021-07-17 14:18:12 -0700

    route for item creation, namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # post "/api/v1/items", to: 'api/v1/items#create'
+  namespace :api do
+    namespace :v1 do
+      # post "items", to: 'items#create'
+      resources :items, only: [:create]
+    end
+  end
 end
