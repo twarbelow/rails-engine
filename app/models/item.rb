71ea6bfe7c86be1a34e2387aed2b4c4f76294f4f
twarbelow <taija.warbelow@gmail.com>
2021-07-17 15:44:52 -0700

    numerality

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 class Item < ApplicationRecord
   validates_presence_of(:name, :description, :unit_price, :merchant_id)
   validates :unit_price, numericality: { greater_than_or_equal_to: 0 }, presence: true
