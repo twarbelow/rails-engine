1ee0954ccd2e07f9ef2ebdba69f71f435fab48d3
twarbelow <taija.warbelow@gmail.com>
2021-07-17 16:17:38 -0700

    extra attributes and missing attribute spec

--- spec/requests/api/v1/items/create_spec.rb
+++ spec/requests/api/v1/items/create_spec.rb
@@ -7,9 +7,40 @@ RSpec.describe 'item creation' do
 
     post api_v1_items_path, params: json_body
 
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(201)
 
-    reply = JSON.parse(response.body)
+    reply = JSON.parse(response.body, symbolize_names: true)
 
+    expect(reply).to have_key(:data)
+    expect(reply[:data]).to have_key(:id)
+    expect(reply[:data]).to have_key(:type)
+    expect(reply[:data]).to have_key(:attributes)
+    expect(reply[:data][:attributes]).to have_key(:name)
+    expect(reply[:data][:attributes]).to have_key(:description)
+    expect(reply[:data][:attributes]).to have_key(:unit_price)
+  end
+
+  it 'ignores any extra attributes sent in the request body' do
+    merchant = create(:merchant)
+    json_body = { "name": "new item", "description": "this is a new item", "unit_price": 1, "merchant_id": merchant.id, "pickles": "pickles"}
+
+    post api_v1_items_path, params: json_body
+
+    expect(response.status).to eq(201)
+
+    reply = JSON.parse(response.body, symbolize_names: true)
+
+    expect(reply).to_not have_key(:pickles)
+    expect(reply[:data]).to_not have_key(:pickles)
+    expect(reply[:data][:attributes]).to_not have_key(:pickles)
+  end
+
+  it 'returns an error if an expected attribute is missing from the request body' do
+    merchant = create(:merchant)
+    json_body = { "name": "new item", "unit_price": 1, "merchant_id": merchant.id}
+
+    post api_v1_items_path, params: json_body
+    
+    expect(response.status).to eq(400)
   end
 end
