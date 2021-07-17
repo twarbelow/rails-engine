a1e732a03d75e17a85b557a3453847c6ac1e90e8
twarbelow <taija.warbelow@gmail.com>
2021-07-17 11:07:24 -0700

    add items to merchant relationships, spec passing

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Merchant, type: :model do
   describe 'relationships' do
     it { should validate_presence_of(:name) }
+    it { should have_many(:items) }
     # it { should have_many(:invoices) }
-    # it { should have_many(:items) }
     # it { should have_many(:transactions).though(:invoices) }
   end
 end
