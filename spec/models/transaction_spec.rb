b3f1da714f55bfcd9ad6adbff889d21f21dcfc0b
twarbelow <taija.warbelow@gmail.com>
2021-07-17 13:28:33 -0700

    transaction spec

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  it { should validate_presence_of(:invoice_id) }
+  it { should validate_presence_of(:credit_card_number) }
+  it { should validate_presence_of(:credit_card_expiration) }
+  it { should validate_presence_of(:result) }
+
+  it { should belong_to(:invoice) }
+end
