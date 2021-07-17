03512c287ff62464f71bcc35f34603cf067c6ba9
twarbelow <taija.warbelow@gmail.com>
2021-07-17 14:12:55 -0700

    item creation test

--- spec/requests/api/v1/items/create_spec.rb
+++ spec/requests/api/v1/items/create_spec.rb
@@ -1,3 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 
+RSpec.describe 'item creation' do
+  it 'accepts json body and post to create' do
+    merchant = create(:merchant)
+    json_body = { "name": "new item", "description": "this is a new item", "unit_price": 1, "merchant_id": merchant.id}
+
+    post api_v1_items_path, params: json_body
+
+    reply = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+  end
+end
