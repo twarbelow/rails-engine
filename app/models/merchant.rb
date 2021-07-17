b58f93af5f73c3b0cc3667e58a716c409cd0123b
twarbelow <taija.warbelow@gmail.com>
2021-07-17 11:16:36 -0700

    add invoice to merchant model and spec, spec passing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,6 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
-  # has_many :invoices
+  has_many :invoices
   # has_many :transactions, through: :invoices
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,7 +4,7 @@ describe Merchant, type: :model do
   describe 'relationships' do
     it { should validate_presence_of(:name) }
     it { should have_many(:items) }
-    # it { should have_many(:invoices) }
+    it { should have_many(:invoices) }
     # it { should have_many(:transactions).though(:invoices) }
   end
 end
