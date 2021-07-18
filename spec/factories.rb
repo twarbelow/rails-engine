babba14e5ae894e8e2abb3c6c4492c2f0f948d63
twarbelow <taija.warbelow@gmail.com>
2021-07-17 17:23:45 -0700

    add item factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,11 @@ FactoryBot.define do
   factory :merchant do
     name { Faker::Name.name }
   end
+
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::Marketing.buzzwords }
+    unit_price { Faker::Commerce.price }
+    association :merchant
+  end
 end
