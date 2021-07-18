bafcdcd55643f8f9f32e6c08328b38abdbb218e0
twarbelow <taija.warbelow@gmail.com>
2021-07-17 20:01:20 -0700

    add destroy to items resource routing

--- spec/requests/api/v1/items/delete_spec.rb
+++ spec/requests/api/v1/items/delete_spec.rb
@@ -4,15 +4,17 @@ RSpec.describe 'delete item' do
   it 'destroys an item when provided a valid item id' do
     item = create(:item)
 
-    DELETE "api/v1/items/#{item.id}"
+    delete "api/v1/items/#{item.id}"
 
     expect(response.status).to eq(204)
-
-    expect(item).to be(nil)
-
-    expect(response.body).to eq(nil)
   end
 
-  it 'deletes associated invoices where the item is the only item on that invoice' do
+  it 'sends 404 if the requested item id is not in the database' do
+    delete "api/v1/items/1234567890987654321"
+
+    expect(response.status).to eq(404)
   end
+
+  # it 'deletes associated invoices where the item is the only item on that invoice' do
+  # end
 end
