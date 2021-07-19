c07ca5b8e7ece8bd8c29c739b5f7afd93a26655d
twarbelow <taija.warbelow@gmail.com>
2021-07-18 21:04:16 -0700

    spec for one merchant endpoint

---
+++ spec/requests/api/v1/merchants/get_one_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'get one merchant' do
+  it 'returns merchant information when provided a valid id' do
+    merchant = create(:merchant)
+    get api_v1_merchant_path(merchant.id)
+
+    expect(response.status).to eq(200)
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data][:id]).to eq(merchant.id)
+    expect(reply[:data][:type]).to eq("merchant")
+    expect(reply[:data][:attributes][:name]).to eq(merchant.name)
+  end
+
+  it 'returns 404 when provided with invalid id integer' do
+    get "/api/v1/merchants/123234345"
+
+    expect(response.status).to eq(404)
+  end
+
+  it 'returns 404 when provided with string for id' do
+    get "/api/v1/merchants/these-are-words"
+
+    expect(response.status).to eq(404)
+  end
+end
