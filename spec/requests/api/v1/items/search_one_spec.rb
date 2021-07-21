30f376ffdd94198892726d3b40260c97c1a75c6a
twarbelow <taija.warbelow@gmail.com>
2021-07-20 20:37:46 -0700

    fill out spec

--- spec/requests/api/v1/items/search_one_spec.rb
+++ spec/requests/api/v1/items/search_one_spec.rb
@@ -20,39 +20,91 @@ RSpec.describe 'search for an item' do
     expect(reply[:data][:attributes][:description]).to eq(item1.description)
     expect(reply[:data][:attributes][:unit_price]).to eq(item1.unit_price)
     expect(reply[:data][:attributes][:merchant_id]).to eq(item1.merchant_id)
-
-
-    # results should be in case-sensitive alphabetical order
-    # the search data in the merchant_id query parameter should require the database to do a case-insensitive search for text fields (names and descriptions!)
   end
 
-  # it 'can search by min price' do
-  #   get "/api/v1/items/find?min_price=50"
-  # end
-  #
-  # it 'can search by max price' do
-  #   get "/api/v1/items/find?max_price=150"
-  # end
-  #
-  # it 'can search by min and max price' do
-  #   get "/api/v1/items/find?max_price=150&min_price=50"
-  # end
-  #
-  # it 'returns an empty response if zero items match the search'
-  #   get "/api/v1/items/find?max_price=0"
-  #
-  # it 'cannot search by name and price' do
-  #   get "/api/v1/items/find?name=ring&min_price=50"
-  # end
-  #
-  # it 'cannot responsd to invalid params' do
-  # end
-end
+  it 'can search by min price' do
+    merchant = create(:merchant)
+    item1 = Item.create!(name: "Ring", description: "Thing that's round", unit_price: 100 , merchant_id: merchant.id )
+    Item.create!(name: "ring", description: "lower case thing that's round", unit_price: 20 , merchant_id: merchant.id )
+    Item.create!(name: "Thing", description: "bring the things", unit_price: 10 , merchant_id: merchant.id )
+    Item.create!(name: "Rocking Horse", description: "certainly not uninspiring", unit_price: 10 , merchant_id: merchant.id )
 
-# results should be in case-sensitive alphabetical order
-# the search data in the name query parameter should require the database to do a case-insensitive search for text fields (names and descriptions!)
-# allow the user to send one or more price-related query parameters, both min_price and max_price can be sent
-# the user may send EITHER the name parameter OR either/both of the price parameters; users should get an error if name and either/both of the price parameters are sent
-# respond with { "data": [ { "id": "4", "type": "item", "attributes": { REGULAR ITEM ATTRIBUTES } }, { "id": "1", "type": "item", "attributes": { REGULAR ITEM ATTRIBUTES } } ] }
-# JSON response will always be an array of objects, even if zero matches or only one match is found.
-# should not return a 404 if no matches are found.
+    get "/api/v1/items/find?min_price=50"
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data][:id]).to eq(item1.id.to_s)
+    expect(reply[:data][:type]).to eq("item")
+    expect(reply[:data][:attributes][:name]).to eq(item1.name)
+    expect(reply[:data][:attributes][:description]).to eq(item1.description)
+    expect(reply[:data][:attributes][:unit_price]).to eq(item1.unit_price)
+    expect(reply[:data][:attributes][:merchant_id]).to eq(item1.merchant_id)
+  end
+
+  it 'can search by max price' do
+    merchant = create(:merchant)
+    item1 = Item.create!(name: "Ring", description: "Thing that's round", unit_price: 100 , merchant_id: merchant.id )
+    Item.create!(name: "ring", description: "lower case thing that's round", unit_price: 20 , merchant_id: merchant.id )
+    Item.create!(name: "Thing", description: "bring the things", unit_price: 10 , merchant_id: merchant.id )
+    Item.create!(name: "Rocking Horse", description: "certainly not uninspiring", unit_price: 10 , merchant_id: merchant.id )
+
+    get "/api/v1/items/find?max_price=150"
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data][:id]).to eq(item1.id.to_s)
+    expect(reply[:data][:type]).to eq("item")
+    expect(reply[:data][:attributes][:name]).to eq(item1.name)
+    expect(reply[:data][:attributes][:description]).to eq(item1.description)
+    expect(reply[:data][:attributes][:unit_price]).to eq(item1.unit_price)
+    expect(reply[:data][:attributes][:merchant_id]).to eq(item1.merchant_id)
+  end
+
+  it 'can search by min and max price' do
+    merchant = create(:merchant)
+    item1 = Item.create!(name: "Ring", description: "Thing that's round", unit_price: 100 , merchant_id: merchant.id )
+    Item.create!(name: "ring", description: "lower case thing that's round", unit_price: 80 , merchant_id: merchant.id )
+    Item.create!(name: "Thing", description: "bring the things", unit_price: 10 , merchant_id: merchant.id )
+    Item.create!(name: "Rocking Horse", description: "certainly not uninspiring", unit_price: 70 , merchant_id: merchant.id )
+
+    get "/api/v1/items/find?max_price=150&min_price=50"
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data][:id]).to eq(item1.id.to_s)
+    expect(reply[:data][:type]).to eq("item")
+    expect(reply[:data][:attributes][:name]).to eq(item1.name)
+    expect(reply[:data][:attributes][:description]).to eq(item1.description)
+    expect(reply[:data][:attributes][:unit_price]).to eq(item1.unit_price)
+    expect(reply[:data][:attributes][:merchant_id]).to eq(item1.merchant_id)
+
+  end
+
+  it 'returns an empty response if zero items match the search' do
+    get "/api/v1/items/find?max_price=0"
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(response.status).to eq(404)
+
+    expect(reply[:data][:id]).to eq(nil)
+    expect(reply[:data][:type]).to eq("item")
+    expect(reply[:data][:attributes][:name]).to eq(nil)
+    expect(reply[:data][:attributes][:description]).to eq(nil)
+    expect(reply[:data][:attributes][:unit_price]).to eq(nil)
+    expect(reply[:data][:attributes][:merchant_id]).to eq(nil)
+
+  end
+  
+  it 'cannot search by name and price' do
+    get "/api/v1/items/find?name=ring&min_price=50"
+
+    expect(response.status).to eq(400)
+  end
+
+  it 'cannot respond to invalid params' do
+    get "/api/v1/items/find?pandas=ring&cats=1000"
+
+    expect(response.status).to eq(400)
+  end
+end
