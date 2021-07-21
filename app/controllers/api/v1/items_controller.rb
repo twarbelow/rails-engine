9fc94b439e56633722be1013a4ca8e76d6ee9caf
twarbelow <taija.warbelow@gmail.com>
2021-07-20 20:29:46 -0700

    logic for prices, find method needs refactor

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
   def find
     if valid_params?
-      item = Item.where("name ILIKE ? OR description ILIKE ?", "%#{params[:name]}%", "%#{params[:name]}%").order(:name).first
-      render json: ItemSerializer.render(item), status: 200
+      if params[:name]
+        # class methods for item "get_by_whatever", pass the params in
+        item = Item.where("name ILIKE ? OR description ILIKE ?", "%#{params[:name]}%", "%#{params[:name]}%").order(:name).first
+      elsif params[:min_price] && !params[:max_price]
+        item = Item.where("unit_price >= ?", params[:min_price]).order(:name).first
+      elsif params[:max_price]
+        item = Item.where("unit_price <= ?", params[:max_price]).order(:name).first
+      else
+        item = Item.where(unit_price: params[:min_price]..params[:max_price]).order(:name).first
+      end
+      if item
+        render json: ItemSerializer.render(item), status: 200
+      else
+        render json: ItemSerializer.render_empty, status: 200
+      end
+    else
+      render status: 400
     end
   end
 
