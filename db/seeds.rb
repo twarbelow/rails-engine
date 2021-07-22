dbf39c81920784f296fbb48944956dff4a0faca0
twarbelow <taija.warbelow@gmail.com>
2021-07-22 08:54:03 -0700

    pg_dump fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+# cmd_1 = "pg_dump --clean"
+cmd_2 = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
 puts "Loading PostgreSQL Data dump into local database with command:"
-puts cmd
-system(cmd)
+# puts cmd_1
+# system(cmd_1)
+puts cmd_2
+system(cmd_2)
