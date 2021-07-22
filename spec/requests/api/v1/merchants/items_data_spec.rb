afe308c6315b96e628d9bf057b1f6c362eda213b
twarbelow <taija.warbelow@gmail.com>
2021-07-22 14:58:24 -0700

    flesh out spec

--- spec/requests/api/v1/merchants/items_data_spec.rb
+++ spec/requests/api/v1/merchants/items_data_spec.rb
@@ -4,22 +4,25 @@ RSpec.describe '/api/v1/merchants/:id/items' do
   it 'returns all items for a valid merchant id' do
     merchant = create(:merchant)
     other_merchant = create(:merchant)
-    items = create_list(:items, 10, merchant_id: merchant.id)
-    create_list(:items, 3, merchant_id: other_merchant.id)
+    items = create_list(:item, 10, merchant_id: merchant.id)
+    create_list(:item, 3, merchant_id: other_merchant.id)
 
     get "/api/v1/merchants/#{merchant.id}/items"
 
     expect(response.status).to eq(200)
 
-    expect(reply[:data].count).to eq(items.count)
+    reply = JSON.parse(response.body, symbolize_names: :true)
 
-    reply[:data].each do |item|
-      expect(item).to have_keys(:id, :type, :attributes)
-      expect(item.keys).to_not be(nil)
-    end
+    expect(reply[:data].count).to eq(items.count)
+    expect(reply[:data][0][:id]).to eq(items[0].id.to_s)
+    expect(reply[:data][0][:attributes][:name]).to eq(items[0].name)
+    expect(reply[:data][0][:attributes][:description]).to eq(items[0].description)
+    expect(reply[:data][0][:attributes][:unit_price]).to eq(items[0].unit_price)
+    expect(reply[:data][0][:attributes][:merchant_id]).to eq(items[0].merchant_id)
   end
+
   it 'returns 404 if merchant is not found' do
-    get "/api/v1/items/123234/merchant"
+    get "/api/v1/merchants/123234/items"
 
     expect(response.status).to eq(404)
   end
