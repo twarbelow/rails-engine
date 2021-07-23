0963a7760a3cc67c0fd15a193df4ec1d1db66c51
twarbelow <taija.warbelow@gmail.com>
2021-07-22 21:02:17 -0700

    formatting

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::RevenueController < ApplicationController
   def total_for_merchant
     merchant = Merchant.find(params[:id])
-    revenue = merchant.invoices.joins(:transactions, :invoice_items).where(
-      'transactions.result = ? AND invoices.status = ?', 'success', 'shipped'
-    ).sum('invoice_items.unit_price * invoice_items.quantity')
+    revenue = merchant.invoices
+                      .joins(:transactions, :invoice_items)
+                      .where('transactions.result = ? AND invoices.status = ?', 'success', 'shipped')
+                      .sum('invoice_items.unit_price * invoice_items.quantity')
     render json: RevenueSerializer.merchant_total(merchant.id, revenue)
   end
 end
