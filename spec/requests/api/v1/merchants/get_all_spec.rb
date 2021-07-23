4147c3d2a4210fde75cb0c9a1665c3a44b89a9a3
twarbelow <taija.warbelow@gmail.com>
2021-07-22 17:20:19 -0700

    pagination and index for merchants

--- spec/requests/api/v1/merchants/get_all_spec.rb
+++ spec/requests/api/v1/merchants/get_all_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/merchants?per_page=<numbers>&page=<number>' do
   it 'defaults to return 20 merchants per page, starting on page 1' do
-    merchants = create_list(:item, 100)
+    merchants = create_list(:merchant, 100)
 
     get "/api/v1/merchants"
 
@@ -15,7 +15,7 @@ RSpec.describe '/api/v1/merchants?per_page=<numbers>&page=<number>' do
   end
 
   it 'returns the specified number of results per page, starting on the specified page number' do
-    merchants = create_list(:item, 6)
+    merchants = create_list(:merchant, 6)
 
     get "/api/v1/merchants?per_page=5&page=2"
 
@@ -37,7 +37,7 @@ RSpec.describe '/api/v1/merchants?per_page=<numbers>&page=<number>' do
   end
 
   it 'returns an empty array if the user requests a page that does not have data' do
-    create_list(:item, 10)
+    create_list(:merchant, 10)
 
     get "/api/v1/merchants?per_page=10&page=1000"
 
