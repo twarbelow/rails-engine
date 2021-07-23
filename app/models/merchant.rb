e8e94c2f1d7874e7ee854e4678cfda3aeee1c75e
twarbelow <taija.warbelow@gmail.com>
2021-07-22 21:09:28 -0700

    refactor merchants_controller#index, add merchant#paginated_merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,13 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
   # has_many :transactions, through: :invoices
+
+  def self.paginated_merchants(page, per_page)
+    if page.zero?
+      Merchant.limit(per_page)
+    else
+      Merchant.limit(per_page)
+              .offset((page - 1) * per_page)
+    end
+  end
 end
