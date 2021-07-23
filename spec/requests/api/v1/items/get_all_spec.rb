0494ba2d1f916d7c6c53f82dc56657f00c781a94
twarbelow <taija.warbelow@gmail.com>
2021-07-22 17:06:27 -0700

    sad paths

--- spec/requests/api/v1/items/get_all_spec.rb
+++ spec/requests/api/v1/items/get_all_spec.rb
@@ -14,16 +14,38 @@ RSpec.describe '/api/v1/items?per_page=<numbers>&page=<number>' do
     expect(reply[:data][0][:id]).to eq(items[0].id.to_s)
   end
 
-  xit 'returns the specified number of results per page, starting on the specified page number' do
+  it 'returns the specified number of results per page, starting on the specified page number' do
+    items = create_list(:item, 6)
 
+    get "/api/v1/items?per_page=5&page=2"
+
+    expect(response.status).to eq(200)
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data][0][:id]).to eq(items.last.id.to_s)
   end
 
-  xit 'returns an empty array if there is nothing to return' do
+  it 'returns an empty array if there is nothing to return' do
+    get "/api/v1/items"
 
+    expect(response.status).to eq(200)
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data]).to match_array([])
   end
 
-  xit 'returns an empty array if the user requests a page that does not have data' do
+  it 'returns an empty array if the user requests a page that does not have data' do
+    create_list(:item, 10)
 
+    get "/api/v1/items?per_page=10&page=1000"
+
+    expect(response.status).to eq(200)
+
+    reply = JSON.parse(response.body, symbolize_names: :true)
+
+    expect(reply[:data]).to match_array([])
   end
 end
 # NOT include dependent data of the resource (eg, if you’re fetching merchants, do not send any data about merchant’s items or invoices)
